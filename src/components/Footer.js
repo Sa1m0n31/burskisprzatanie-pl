c2bad8a228d696903e1c708c667ed4e91962d149
Szymon Burak <sajmon0031@gmail.com>
2020-10-22 01:01:01 +0200

    Change authors name

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (<footer>
         <h4>Polityka prywatności</h4><span className="desktopOnly">|</span>
         <h4 className="middle">&copy; burskisprzatanie.pl</h4><span className="desktopOnly">|</span>
-        <h4>Projekt i realizacja <a href="https://stronnnka.pl" target="_blank" rel="noreferrer">stronnnka.pl</a></h4>
+        <h4>Projekt i realizacja <a href="https://skylo.pl" target="_blank" rel="noreferrer">Skylo.pl</a></h4>
     </footer>)
 };
 
